70610dfba517d22696c23388ce79883abade031e
ren-oak <1024479225@qq.com>
2013-11-12 01:17:55 +0800

    服务器端

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverport = new System.Windows.Forms.TextBox();
+            this.showinfo = new System.Windows.Forms.RichTextBox();
+            this.startService = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statuBar = new System.Windows.Forms.ToolStripStatusLabel();
+            this.userList = new System.Windows.Forms.ListBox();
+            this.butExit = new System.Windows.Forms.Button();
+            this.serCle = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口号：";
+            // 
+            // serverport
+            // 
+            this.serverport.Location = new System.Drawing.Point(62, 40);
+            this.serverport.Name = "serverport";
+            this.serverport.Size = new System.Drawing.Size(54, 21);
+            this.serverport.TabIndex = 1;
+            this.serverport.Text = "5678";
+            // 
+            // showinfo
+            // 
+            this.showinfo.Location = new System.Drawing.Point(12, 65);
+            this.showinfo.Name = "showinfo";
+            this.showinfo.Size = new System.Drawing.Size(270, 266);
+            this.showinfo.TabIndex = 2;
+            this.showinfo.Text = "";
+            // 
+            // startService
+            // 
+            this.startService.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.startService.Location = new System.Drawing.Point(12, 0);
+            this.startService.Name = "startService";
+            this.startService.Size = new System.Drawing.Size(121, 34);
+            this.startService.TabIndex = 3;
+            this.startService.Text = "开始服务";
+            this.startService.UseVisualStyleBackColor = true;
+            this.startService.Click += new System.EventHandler(this.startService_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statuBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 369);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(287, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statuBar
+            // 
+            this.statuBar.Name = "statuBar";
+            this.statuBar.Size = new System.Drawing.Size(72, 17);
+            this.statuBar.Text = "未启动服务";
+            // 
+            // userList
+            // 
+            this.userList.FormattingEnabled = true;
+            this.userList.ItemHeight = 12;
+            this.userList.Location = new System.Drawing.Point(122, 47);
+            this.userList.Name = "userList";
+            this.userList.Size = new System.Drawing.Size(11, 4);
+            this.userList.TabIndex = 7;
+            // 
+            // butExit
+            // 
+            this.butExit.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.butExit.Location = new System.Drawing.Point(189, 337);
+            this.butExit.Name = "butExit";
+            this.butExit.Size = new System.Drawing.Size(93, 25);
+            this.butExit.TabIndex = 11;
+            this.butExit.Text = "退出\r\n";
+            this.butExit.UseVisualStyleBackColor = true;
+            this.butExit.Click += new System.EventHandler(this.butExit_Click);
+            // 
+            // serCle
+            // 
+            this.serCle.Location = new System.Drawing.Point(14, 337);
+            this.serCle.Name = "serCle";
+            this.serCle.Size = new System.Drawing.Size(91, 25);
+            this.serCle.TabIndex = 14;
+            this.serCle.Text = "清空";
+            this.serCle.UseVisualStyleBackColor = true;
+            this.serCle.Click += new System.EventHandler(this.serCle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 391);
+            this.Controls.Add(this.serCle);
+            this.Controls.Add(this.butExit);
+            this.Controls.Add(this.userList);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.startService);
+            this.Controls.Add(this.showinfo);
+            this.Controls.Add(this.serverport);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "服务器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverport;
+        private System.Windows.Forms.RichTextBox showinfo;
+        private System.Windows.Forms.Button startService;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statuBar;
+        private System.Windows.Forms.ListBox userList;
+        private System.Windows.Forms.Button butExit;
+        private System.Windows.Forms.Button serCle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Text.RegularExpressions;
+ 
+namespace TCPServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+        public bool btnstatu = true;  //开始停止服务按钮状态
+        public Thread myThread;       //声明一个线程实例
+        public Socket newsock;        //声明一个Socket实例
+        public Socket server1;
+        public Socket Client;
+        public delegate void MyInvoke(string str);
+        public IPEndPoint localEP;    
+        public int localPort;
+        public EndPoint remote;
+        public Hashtable _sessionTable;      
+        public bool m_Listening;
+        private static byte[] result = new byte[1024];
+        Socket serial;
+        //用来设置服务端监听的端口号
+        public int setPort            
+        {
+            get { return localPort; }
+            set { localPort = value; }
+        }       
+        //用来往richtextbox框中显示消息
+        public void showClientMsg(string msg)
+        {
+            //在线程里以安全方式调用控件
+            if (showinfo.InvokeRequired)
+            {
+                MyInvoke _myinvoke = new MyInvoke(showClientMsg);
+                showinfo.Invoke(_myinvoke, new object[] { msg });               
+            }
+            else
+            {
+                showinfo.AppendText(msg);             
+            }           
+        }
+      public void userListOperate(string msg)
+        {
+            //在线程里以安全方式调用控件
+            if (userList.InvokeRequired)
+            {
+                MyInvoke _myinvoke = new MyInvoke(userListOperate);
+                userList.Invoke(_myinvoke, new object[] { msg });
+            }
+            else
+            {
+                userList.Items.Add(msg);
+            }
+        }
+        public void userListOperateR(string msg)
+        {
+            //在线程里以安全方式调用控件
+            if (userList.InvokeRequired)
+            {
+                MyInvoke _myinvoke = new MyInvoke(userListOperateR);
+                userList.Invoke(_myinvoke, new object[] { msg });
+            }
+            else
+            {
+                userList.Items.Remove(msg);
+            }
+        }
+        //监听函数
+        public void Listen()
+        {   //设置端口
+            setPort=int.Parse(serverport.Text.Trim());
+            //初始化SOCKET实例
+            newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //允许SOCKET被绑定在已使用的地址上。
+            newsock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //初始化终结点实例
+            localEP=new IPEndPoint(IPAddress.Any,setPort);
+            try
+            {
+                _sessionTable = new Hashtable(53);
+                //绑定
+                newsock.Bind(localEP);
+                //监听
+                newsock.Listen(10);
+                Thread myThread = new Thread(ListenClientConnect);
+                myThread.Start();
+               //开始接受连接，异步。
+                //newsock.BeginAccept(new AsyncCallback(OnConnectRequest), newsock);
+             }
+            catch (Exception ex)
+            {
+                showClientMsg(ex.Message);
+            }
+        }
+      public void ListenClientConnect()
+      {
+
+          string msg_str = "";
+          string msg_str1 = "@client@";
+          string msg_str2 = "@serialassistant@";
+          while (true)
+          {
+              Socket clientSocket = newsock.Accept();
+              DateTimeOffset now = DateTimeOffset.Now;
+              string strDateLine = "欢迎登录到服务器"+"\r\n";
+              Byte[] byteDateLine = System.Text.Encoding.UTF8.GetBytes(strDateLine);
+              remote = clientSocket.RemoteEndPoint;
+              showClientMsg(remote.ToString() + "连接成功。" + now.ToString("G")+"\r\n");
+              clientSocket.Send(byteDateLine, byteDateLine.Length, 0);        
+              int receiveNumber = clientSocket.Receive(byteDateLine);
+              msg_str = Encoding.UTF8.GetString(byteDateLine);
+              if (msg_str.Contains(msg_str1)==true)                                
+             {               
+                  //把连接成功的客户端的socket实例放到哈希表
+                  _sessionTable.Add(clientSocket, remote);               
+                  Thread receiveThread = new Thread(ReceiveMessage);
+                  receiveThread.Start(clientSocket);                      
+              }
+              else if (msg_str.Contains(msg_str2) == true)
+              {                
+                  serial = clientSocket;
+                  Thread receiveThread = new Thread(ReceiveMessage);
+                  receiveThread.Start(clientSocket);
+              }
+              else
+              {                                
+              } 
+          }
+      }
+      public void ReceiveMessage(object clientSocket)
+      {
+          Socket myClientSocket = (Socket)clientSocket;
+          while (true)
+          {
+              try
+              {
+                  string strDateLine = "欢迎登录到服务器";
+                  Byte[] byteDateLine = System.Text.Encoding.UTF8.GetBytes(strDateLine);                 
+                  int receiveNumber = myClientSocket.Receive(byteDateLine);                 
+                  string stringdata = Encoding.UTF8.GetString(byteDateLine, 0, receiveNumber);
+                  string ip = myClientSocket.RemoteEndPoint.ToString();                  
+                  if (_sessionTable.Contains(myClientSocket))
+                  {
+                      
+                      showClientMsg("get client mesg"+"("+ip+")"+":" + stringdata + ",will send to:" + serial.RemoteEndPoint.ToString() + "\r\n");       
+                      Byte[] sendData = Encoding.UTF8.GetBytes(stringdata);
+                      serial.SendTo(sendData, serial.RemoteEndPoint);
+                  }
+                  else {                      
+                        showClientMsg(stringdata);
+                       SendBroadMsg(stringdata);                 
+                  }               
+              }
+              catch 
+              {
+                  myClientSocket.Shutdown(SocketShutdown.Both);
+                  myClientSocket.Close();
+                  break;
+              }             
+          }
+      }    
+   //以下实现发送广播消息
+     public void SendBroadMsg(string strDataLine)
+      {   
+          Byte[] sendData = Encoding.UTF8.GetBytes(strDataLine);
+          foreach (DictionaryEntry de in _sessionTable)
+          {
+              Socket cli = (Socket)de.Key;
+              EndPoint temp = (EndPoint)de.Value;           
+              cli.SendTo(sendData, temp);             
+          }
+       
+     }  
+      //开始停止服务按钮
+        private void startService_Click(object sender, EventArgs e)
+        {
+            //新建一个委托线程
+           ThreadStart myThreadDelegate = new ThreadStart(Listen);
+            //实例化新线程                                                                                        
+            myThread = new Thread(myThreadDelegate);
+            if (btnstatu)
+            {               
+                myThread.Start();
+                statuBar.Text = "服务已启动，等待客户端连接";
+                btnstatu = false;
+                startService.Text = "停止服务";
+            }
+            else
+            {
+                //停止服务
+                myThread.Interrupt();
+                myThread.Abort();                
+                btnstatu = true;
+                startService.Text = "开始服务";
+                statuBar.Text = "服务已停止";                
+            }            
+        }
+        //窗口关闭时中止线程。
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (myThread != null)
+            {
+                myThread.Abort();
+            }
+        }
+        private void send_Click(object sender, EventArgs e)
+        {
+          //  SendBroadMsg();
+        }     
+        private void butExit_Click(object sender, EventArgs e)
+        {          
+            myThread.Abort();
+            Application.Exit();
+        }
+        private void serCle_Click(object sender, EventArgs e)
+        {
+            showinfo.Text = "";
+        }             
+    }
+}
